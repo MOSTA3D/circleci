7c952eb8d839eda3356ac7b3dd66f78ef812c952
MOSTA3D <khabbieny123@gmail.com>
2022-02-24 14:35:48 +0200

    fixing deployment url

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./build https://s3.console.aws.amazon.com/s3/buckets/elasticbeanstalk-us-east-1-050210338205?region=us-east-1
+aws s3 cp --recursive --acl public-read ./build s3://elasticbeanstalk-us-east-1-050210338205
